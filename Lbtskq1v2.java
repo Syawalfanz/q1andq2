2a11301bb48e052466c04366120009d0c2c78beb
Syawalfanz <151454022+Syawalfanz@users.noreply.github.com>
2023-12-27 08:15:22 +0800

    Update and rename Lbtskq1v1.java to Lbtskq1v2.java

--- Lbtskq1v2.java
+++ Lbtskq1v2.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
-public class Lbtskq1v1 {
+public class Lbtskq1v2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         DecimalFormat decimalFormat = new DecimalFormat("#.00");
